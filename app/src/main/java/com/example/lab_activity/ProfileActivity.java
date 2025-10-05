5fc842a2e398e95246cb8d3cc17585d38c65feb1
Sangdep <Sangletop125@gmail.com>
2025-10-05 09:28:12 +0700

    Activity layout

--- app/src/main/java/com/example/lab_activity/ProfileActivity.java
+++ app/src/main/java/com/example/lab_activity/ProfileActivity.java
@@ -1,6 +1,8 @@
 package com.example.lab_activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,13 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ImageButton imageButton= findViewById(R.id.btnSetting);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
     }
 }
